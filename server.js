a286de23fa704346a29d9b95b38c468eb6d80eb6
hazem-a1 <hazem.m.sobhy@gmail.com>
2020-11-20 17:38:39 +0200

    pass

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
@@ -10,6 +12,16 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.set("view engine", "pug");
 
 app.route("/").get((req, res) => {
