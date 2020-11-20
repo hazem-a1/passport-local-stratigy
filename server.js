0951cf3fcac9f571a8ff5e7fff408721fd903b30
hazem-a1 <hazem.m.sobhy@gmail.com>
2020-11-20 18:20:30 +0200

    pass

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+const LocalStrategy = require("passport-local");
 const ObjectID = require("mongodb").ObjectID;
 
 const myDB = require("./connection");
@@ -35,6 +36,24 @@ app.use(passport.session());
 //  not sending any thing until connecting to DB
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
+
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log("User " + username + " attempted to log in.");
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (password !== user.password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
 
   // Be sure to change the title
   app.route("/").get((req, res) => {
