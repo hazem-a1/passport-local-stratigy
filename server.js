6e3a280cf8b2b4914a893b8c0ff0442e07d881a3
hazem-a1 <hazem.m.sobhy@gmail.com>
2020-11-20 17:47:11 +0200

    pass

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+const ObjectID = require("mongodb").ObjectID;
+
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
@@ -10,8 +12,13 @@ const app = express();
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// set view engine  pug
+app.set("view engine", "pug");
+// use session to store the user
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -20,10 +27,23 @@ app.use(
     cookie: { secure: false },
   })
 );
+
+// init passport with session
 app.use(passport.initialize());
 app.use(passport.session());
-app.set("view engine", "pug");
 
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  //   done(null, null);
+  // });
+  done(null, null);
+});
+
+//  send variables to the pug engine
 app.route("/").get((req, res) => {
   res.render(process.cwd() + "/views/pug/index", {
     title: "Hello",
