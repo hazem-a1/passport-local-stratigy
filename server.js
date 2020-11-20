165a9ddeaceea0249262ec3fc08e7ded4ce906c8
hazem-a1 <hazem.m.sobhy@gmail.com>
2020-11-20 20:10:59 +0200

    pass

--- server.js
+++ server.js
@@ -97,26 +97,27 @@ myDB(async (client) => {
 });
 
 //  send variables to the pug engine
-app.post(
-  "/login",
-  passport.authenticate("local", { failureRedirect: "/" }),
-  (req, res) => {
-    res.redirect("/profile");
-  }
-);
+app
+  .route("/login")
+  .post(
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res) => {
+      res.redirect("/profile");
+    }
+  );
 
 app.route("/profile").get(ensureAuthenticated, (req, res) => {
   res.render("pug/profile", { username: req.user.username });
 });
 
-app.route.get((req, res) => {
+app.route("/logout").get((req, res) => {
   req.logout();
   res.redirect("/");
 });
 
-app.use((req, res, next) => {
-  res.status(404).type("text").send("Not Found");
-});
+// app.use((req, res) => {
+//   res.status(404).type("text").send("Not Found");
+// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
